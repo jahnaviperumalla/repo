57fc540a08c7d10da8a4effbc19eb1a7c07b2138
karthikbabudevops <38009888+karthikbabudevops@users.noreply.github.com>
2022-12-09 17:25:42 +0530

    Update AMI.tf

--- AMI.tf
+++ AMI.tf
@@ -1,7 +1,7 @@
 resource "aws_ami_from_instance" "example" {
   AMI_name           = "AMI_name1"
   source_instance_id = "source_instance_id1"
-  Creation_date      = "Creation_date"
+  Creation_date      = "Creation_date1"
   Platform           = "Platform1"
   Virtualization     = "Virtualization1"
 
