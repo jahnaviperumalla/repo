2fb8e31eb50b59218fe6488c61abeb509c946c5b
karthikbabudevops <38009888+karthikbabudevops@users.noreply.github.com>
2022-12-14 12:02:59 +0530

    Update AMI.tf

--- AMI.tf
+++ AMI.tf
@@ -1,10 +1,6 @@
-resource "aws_ami_from_instance" "example" {
-  AMI_name           = "AMI_name1"
-  source_instance_id = "source_instance_id1"
-  Creation_date      = "Creation_date1"
-  Platform           = "Platform1"
-  Virtualization     = "Virtualization1"
-
+resource "aws_ami_copy" "example" {
+  name              = "terraform-example"
+  description       = "A copy of ami-xxxxxxxx"
+  source_ami_id     = "ami-xxxxxxxx"
+  source_ami_region = "us-west-2"
 }
-
-
