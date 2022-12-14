34177f23434fea586484e600a8493984e0836313
karthikbabudevops <38009888+karthikbabudevops@users.noreply.github.com>
2022-12-14 13:48:00 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,7 +1,19 @@
+resource "aws_route53_zone" "main" {
+  name = "example.io"
+}
+
+resource "aws_route53_zone" "dev" {
+  name = "dev.example.io"
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 resource "aws_route53_record" "dev-ns" {
-  zone_id = "zone_id1"
-  fqdn    = "fqdn1"
-  type    = "type1"
-  ttl     = "ttl1"
-  records = "records1"
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "dev.example.io"
+  type    = "NS"
+  ttl     = "30"
+  records = ["10.0.0.1"]
 }
