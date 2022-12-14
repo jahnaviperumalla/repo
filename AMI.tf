551237f9f0b849c89f9aa49eef8fe6b05f1f37bf
karthikbabudevops <38009888+karthikbabudevops@users.noreply.github.com>
2022-12-14 13:09:58 +0530

    Update AMI.tf

--- AMI.tf
+++ AMI.tf
@@ -1,6 +1,4 @@
-resource "aws_ami_copy" "example" {
-  name              = "name1"
-  description       = "description1"
-  source_ami_id     = "source_ami_id1"
-  source_ami_region = "source_ami_region1"
+resource "aws_ami_from_instance" "example" {
+  name               = "name1"
+  source_instance_id = "source_instance_id1"
 }
