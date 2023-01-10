ab6eae2f51ba2d4a17f0401e6675730500126bc1
karthikbabudevops <38009888+karthikbabudevops@users.noreply.github.com>
2023-01-10 11:57:43 +0530

    Update sample.tf

--- test/sample.tf
+++ test/sample.tf
@@ -1,7 +1,4 @@
-resource "aws_ami_from_instance" "example" {
+resource "aws_ami_from_instance" "exampleAMI" {
   name               = "name1"
   source_instance_id = "source_instance_id1"
-  tags = {
-    Name = "tag_name"
-  }
 }
